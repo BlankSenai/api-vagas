6fe5c2c9a502fe4263ed925b0792df6a6dc2850f
gabrielblank <gabrielpbbl4nk@gmail.com>
2024-10-03 19:45:35 -0300

    fix uppercase letter in seed

--- src/server/database/seeds/0000_insert_space.ts
+++ src/server/database/seeds/0000_insert_space.ts
@@ -10,140 +10,140 @@ export const seed = async (knex: Knex) => {
 
 const spaces = [
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 1
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 2
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 3
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 4
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 5
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 6
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 7
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 8
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 9
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 10
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 11
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 12
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 13
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 14
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 15
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 16
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 17
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 18
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
     numeroVaga: 19
   },
   {
-    status: 'livre',
+    status: 'Livre',
     placa: null,
     blocoAp: null,
     numeroAp: null,
